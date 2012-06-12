f5625e5d89a48ebd61115e63dcb2de7041e13f5e
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-06-12 08:01:37 -0400

    more progess on user integration spec

--- spec/integration/integration_specs.rb
+++ spec/integration/integration_specs.rb
@@ -14,13 +14,12 @@ describe "Create User integration" do
     click_link 'Users'
     click_link 'New user'
     fill_in 'Name', :with => 'Happy Bobappy'
-    fill_in 'Email', :with => 'happy.bobabby@example.com'
+    fill_in 'Email', :with => 'happy.bobappy@example.com'
     fill_in 'Password', :with => 'password'
     click_button 'Create'
-    users_page = page.text.split("\n  ")
-    users_page.must_include "Happy Bobabby"
-    users_page.must_include 'happy.bobabby@example.com'
-    users_page.must_include 'delete'
-    users_page.must_include 'edit'
+    page.text.must_include "Happy Bobappy"
+    page.text.must_include 'happy.bobappy@example.com'
+    page.text.must_include 'delete'
+    #users_page.must_include 'edit'
   end
 end

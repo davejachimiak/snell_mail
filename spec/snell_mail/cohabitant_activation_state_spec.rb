b8b9d46c5ee3f5dbcb386da89ecd19480dae879d
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-08-05 22:22:17 -0400

    Set up more for cohabitant activiation state spec

--- spec/snell_mail/cohabitant_activation_state_spec.rb
+++ spec/snell_mail/cohabitant_activation_state_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 require_relative '../../lib/snell_mail/cohabitant_activation_state.rb'
 
 describe 'CohabitantActivationState' do
+  describe '#message' do
+    it 'sends correct flash info for activated cohabitant' do
 
+    end
+  end
 end

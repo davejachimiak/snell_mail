4aa49e9345dd55a4d8f5207e4609db852b89566f
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-08-06 14:08:09 -0400

    add new specs and class

---
+++ lib/snell_mail/cohabitant_activation_state.rb
@@ -0,0 +1,27 @@
+module SnellMail
+  class CohabitantActivationState
+    attr_accessor :cohabitant
+
+    def initialize(cohabitant)
+      @cohabitant = cohabitant
+    end
+
+    def info
+      if cohabitant.activated?
+        active_info
+      else
+        inactive_info
+      end
+    end
+    
+    private
+    
+      def active_info
+        { flash: 'success', adj: 'reactivated' }
+      end
+      
+      def inactive_info
+        { flash: 'info', adj: 'deactivated' }
+      end
+  end
+end
---
+++ spec/snell_mail/message_subjects_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require_relative './../../lib/snell_mail/message_subjects.rb'
+
+describe 'MessageSubjects' do
+
+  after do
+    Cohabitant.destroy_all if Cohabitant.any?
+  end
+
+  describe 'new instance' do
+    it 'requires subjects as an argument' do
+      it = Proc.new { SnellMail::MessageSubjects.new }
+      it.must_raise ArgumentError
+    end
+  end
+
+  describe '#construct' do
+    it 'is responsive' do
+      it = SnellMail::MessageSubjects.new(nil)
+      it.must_respond_to :construct
+    end
+
+    describe 'with one subject' do
+      it 'returns neccessary text' do
+        department = [Factory(:cohabitant).department]
+        it = SnellMail::MessageSubjects.new(department)
+
+        it.construct.must_equal('Cool Factory was ')
+      end
+    end
+
+    describe 'with two subjects' do
+      it 'returns neccessary text' do
+        departments = [Factory(:cohabitant).department,
+                       Factory(:cohabitant_3).department]
+        it = SnellMail::MessageSubjects.new(departments)
+
+        it.construct.must_equal('Cool Factory and Face Surgery were ')
+      end
+    end
+
+    describe 'with many subjects' do
+      it "returns necessary text" do
+        departments = [Factory(:cohabitant).department,
+                       Factory(:cohabitant_2).department,
+                       Factory(:cohabitant_3).department]
+        it = SnellMail::MessageSubjects.new(departments)
+
+        it.construct.must_equal('Cool Factory, Jargon House, ' +
+                                'and Face Surgery were ')
+      end
+    end
+  end
+end

76d6062084bb83c8da5903c63b2173be1f8dd084
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-08-08 15:57:09 -0400

    Refactor cohabitants helper

--- app/helpers/cohabitants_helper.rb
+++ app/helpers/cohabitants_helper.rb
@@ -1,13 +1,23 @@
 module CohabitantsHelper
   def toggle_activated_button(cohabitant)
-    if cohabitant.activated?
-      button_info = { text: 'deactivate', html_class: 'btn btn-inverse' }
-    else
-      button_info = { text: 'activate', html_class: 'btn' }
-    end
-    
+    button_info = if cohabitant.activated?
+                    activated_info
+                  else
+                    deactivated_info
+                  end
+
     button_to button_info[:text], toggle_activated_path(cohabitant),
       title: "#{button_info[:text].capitalize} #{cohabitant.department}",
       class: button_info[:html_class]
   end
+
+  private
+
+    def activated_info
+      { text: 'deactivate', html_class: 'btn btn-inverse' }
+    end
+    
+    def deactivated_info
+      { text: 'activate', html_class: 'btn' }
+    end
 end
